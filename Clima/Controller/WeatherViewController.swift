b39193ffc39668c9812cd4fed4d0adaaedce7c32
elahe <el.sh192014@gmail.com>
2022-05-30 12:28:28 +0200

    add textfieldShouldReturn func
    return button in this app is go button

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -26,8 +26,14 @@ class WeatherViewController: UIViewController, UITextFieldDelegate {
     }
 
     @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
         print(searchTextField.text!)
         
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
+        return true
     }
     
 }
