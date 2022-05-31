d8f921ed5ade5e172b6d037dca473d05daeeefcb
elahe <el.sh192014@gmail.com>
2022-05-31 10:45:57 +0200

    add weatherURL

---
+++ Clima/Model/WeatherManager.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by Elahe  Sharifi on 31/05/2022.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherManager {
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=a64d08bd6967da0ccccb95ec1c5e50b1&units=metric"
+}
